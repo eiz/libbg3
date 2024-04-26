aeabf429133b8b179cb3d689d7c1a671cf7aec65
Mackenzie Straight <mack@str.rs>
2024-04-26 16:16:34 -0400

    add a couple of python functions to binding module

--- examples/pybg3.cc
+++ examples/pybg3.cc
@@ -19,3 +19,45 @@
 #include "../libbg3.h"
 
 #include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int osiris_decompile_path(const std::string& src, const std::string& dst) {
+  bg3_status status = bg3_success;
+  bg3_mapped_file file;
+  if ((status = bg3_mapped_file_init_ro(&file, src.c_str()))) {
+    return status;
+  }
+  bg3_osiris_save save;
+  bg3_osiris_save_init_binary(&save, file.data, file.data_len);
+  status = bg3_osiris_save_write_sexp(&save, dst.c_str(), false);
+  bg3_osiris_save_destroy(&save);
+  bg3_mapped_file_destroy(&file);
+  return status;
+}
+
+int osiris_compile_path(const std::string& src, const std::string& dst) {
+  bg3_status status = bg3_success;
+  bg3_mapped_file file;
+  if ((status = bg3_mapped_file_init_ro(&file, src.c_str()))) {
+    return status;
+  }
+  bg3_osiris_save_builder builder;
+  bg3_osiris_save_builder_init(&builder);
+  status = bg3_osiris_save_builder_parse(&builder, file.data, file.data_len);
+  if (!status) {
+    status = bg3_osiris_save_builder_finish(&builder);
+  }
+  if (!status) {
+    status = bg3_osiris_save_write_binary(&builder.save, dst.c_str());
+  }
+  bg3_osiris_save_builder_destroy(&builder);
+  bg3_mapped_file_destroy(&file);
+  return status;
+}
+
+PYBIND11_MODULE(_pybg3, m) {
+  m.doc() = "python libbg3 bindings";
+  m.def("osiris_compile_path", &osiris_compile_path, "Compile an osiris save");
+  m.def("osiris_decompile_path", &osiris_decompile_path, "Decompile an osiris save");
+}
